4d31ad46ed2a4c2a4f861398f197f1bfb508de11
hazae41 <hazae41@gmail.com>
2018-09-22 19:42:22 +0200

    4.0: beta

--- src/Sockets4MC.kt
+++ src/Sockets4MC.kt
@@ -1,36 +1,49 @@
 package fr.rhaz.minecraft
 
+import com.google.gson.JsonParser
 import fr.rhaz.sockets.*
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.ChatColor.*
+import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.api.event.PostLoginEvent
+import net.md_5.bungee.api.plugin.Listener as BungeeListener
 import net.md_5.bungee.config.Configuration
 import net.md_5.bungee.config.ConfigurationProvider
+import net.md_5.bungee.event.EventHandler as BungeeEventHandler
 import net.md_5.bungee.config.YamlConfiguration as BungeeYaml
 import org.bukkit.Bukkit
 import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.event.EventHandler as BukkitEventHandler
 import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener as BukkitListener
+import org.bukkit.event.player.PlayerJoinEvent
 import java.io.File
 import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
 import java.nio.file.Files
+import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
 import net.md_5.bungee.api.plugin.Event as BungeeEvent
 import net.md_5.bungee.api.plugin.Plugin as BungeePlugin
 import org.bukkit.event.Event as BukkitEvent
 import org.bukkit.plugin.java.JavaPlugin as BukkitPlugin
-import org.spongepowered.api.plugin.Plugin as SpongePlugin
 
 open class Sockets4Bukkit: BukkitPlugin() {
-    val sockets = Sockets4MC()
     override fun onEnable() = sockets.bukkit(this)
     override fun onDisable() = sockets.disable()
 }
 
 open class Sockets4Bungee: BungeePlugin() {
-    val sockets = Sockets4MC()
     override fun onEnable() = sockets.bungee(this)
     override fun onDisable() = sockets.disable()
 }
 
-class Sockets4MC(){
+val sockets = Sockets4MC()
+class Sockets4MC {
     lateinit var plugin: Any
     lateinit var environment: String
     var logger: Logger = Logger.getGlobal()
@@ -48,6 +61,7 @@ class Sockets4MC(){
 
     fun bungee(plugin: Sockets4Bungee){
         this.plugin = plugin
+        plugin.update(15938, LIGHT_PURPLE)
         environment = "bungee"
         logger = plugin.logger
         dataFolder = plugin.dataFolder
@@ -95,6 +109,7 @@ class Sockets4MC(){
 
     fun bukkit(plugin: Sockets4Bukkit){
         this.plugin = plugin
+        plugin.update(15938, LIGHT_PURPLE)
         environment = "bukkit"
         logger = plugin.logger
         dataFolder = plugin.dataFolder
@@ -361,6 +376,76 @@ fun BukkitPlugin.load(file: File): YamlConfiguration? {
     val res = file.nameWithoutExtension+"/bukkit.yml"
     if (!file.exists()) Files.copy(getResource(res), file.toPath())
     return YamlConfiguration.loadConfiguration(file) ?: null;
+}
+
+fun CommandSender.msg(msg: String) = msg(text(msg))
+fun CommandSender.msg(text: TextComponent) = sendMessage(text)
+fun text(string: String) = TextComponent(string.replace("&", "§"))
+
+fun spiget(id: Int): String = try {
+    val base = "https://api.spiget.org/v2/resources/"
+    val conn = URL("$base$id/versions?size=100").openConnection()
+    val json = InputStreamReader(conn.inputStream).let{ JsonParser().parse(it).asJsonArray}
+    json.last().asJsonObject["name"].asString
+} catch(e: IOException) {e.printStackTrace(); "0"}
+
+infix fun String.newerThan(v: String): Boolean = false.also{
+    val s1 = split('.');
+    val s2 = v.split('.');
+    for(i in 0..Math.max(s1.size,s2.size)){
+        if(i !in s1.indices) return false;
+        if(i !in s2.indices) return true;
+        if(s1[i].toInt() > s2[i].toInt()) return true;
+        if(s1[i].toInt() < s2[i].toInt()) return false;
+    }
+}
+
+fun BukkitPlugin.update(id: Int, color: ChatColor) {
+
+    if(!(spiget(id) newerThan description.version)) return;
+
+    val message = text("An update is available for ${description.name}!").apply {
+        val url = "https://www.spigotmc.org/resources/$id"
+        text += " Download it here: $url"
+        this.color = color
+        clickEvent = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+    }
+
+    server.scheduler.runTaskLater(this, {
+        server.consoleSender.spigot().sendMessage(message);
+    }, 0)
+
+    server.pluginManager.registerEvents(object: BukkitListener {
+        @BukkitEventHandler
+        fun onJoin(e: PlayerJoinEvent) {
+            if(e.player.hasPermission("rhaz.update"))
+                e.player.spigot().sendMessage(message)
+        }
+    }, this)
+}
+
+fun BungeePlugin.update(id: Int, color: ChatColor) {
+
+    if(!(spiget(id) newerThan description.version)) return;
+
+    val message = text("An update is available for ${description.name}!").apply {
+        val url = "https://www.spigotmc.org/resources/$id"
+        text += "\nDownload it here: $url"
+        this.color = color
+        clickEvent = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+    }
+
+    proxy.scheduler.schedule(this, {
+        proxy.console.sendMessage(message);
+    }, 0, TimeUnit.SECONDS)
+
+    proxy.pluginManager.registerListener(this, object : BungeeListener {
+        @BungeeEventHandler
+        fun onJoin(e: PostLoginEvent) {
+            if (e.player.hasPermission("rhaz.update"))
+                e.player.sendMessage(message)
+        }
+    })
 }
 
 val String.lc get() = toLowerCase()
